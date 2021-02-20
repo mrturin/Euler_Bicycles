2dbca94cff2eb75b40998fa0ce7d5150e3b0dc16
Dmitry Polyakov <polds@list.ru>
2021-02-20 18:52:37 +0300

    update tree function

--- Euler_Bicycles/Euler_Bicycles/Tree.cpp
+++ Euler_Bicycles/Euler_Bicycles/Tree.cpp
@@ -3,13 +3,14 @@
      * Description: Определяет методы связанные с выделением дерева, заполнением дерева
 ******************************************************************************/
 
-char** init_tree(int mlet, int M) {
-    char** tree = new char* [M];
+char*** init_tree(int mlet, int M) {
+    char*** tree = new char** [M];
+    char** tree_1 = new char* [M];
 
-    char* zero = new char[4];
+    char* zero = new char[4]{0,0,0,0};
    
     for (int N = 0; N < M; N++) {
-        tree[N] = new char[mlet];
+        tree_1[N] = new char[mlet];
         for (int i = 0; i <= mlet; i++) { tree[N][i] = zero; }
     }
     return tree;
