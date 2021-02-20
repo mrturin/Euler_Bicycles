73c2e8b79bb37cd8eb2c20dcaf7d689a09137390
Dmitry Polyakov <polds@list.ru>
2021-02-20 21:01:30 +0300

    tree fix loop

--- Euler_Bicycles/Euler_Bicycles/Tree.cpp
+++ Euler_Bicycles/Euler_Bicycles/Tree.cpp
@@ -5,14 +5,15 @@
 
 char*** init_tree(int mlet, int M) {
     char*** tree = new char** [M];
-    char* zero = new char[4]{ 0,0,0,0 };
+    //char* zero = new char[4]{ 0,0,0,0 };
     for (int i = 0; i < M; i++)
     {
         tree[i] = new char* [mlet]; // столбцы
-        for (int j = 0; j < mlet; i++)
+        for (int j = 0; j < mlet; j++)
         {
             tree[i][j] = new char[4]; //строки
-            tree[i][j] = zero;
+            for (int z = 0; z < 4; z++)
+                tree[i][j][z] = 0 ;
         }
     }
     return tree;
